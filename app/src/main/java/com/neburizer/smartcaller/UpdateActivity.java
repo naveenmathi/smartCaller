a24c44ac7cdccf913ea266433c7f35b7aa058e00
naveenmathi <naveenmathi1992@gmail.com>
2015-06-10 00:48:00 +0530

    callLog included need to split days

--- app/src/main/java/com/neburizer/smartcaller/UpdateActivity.java
+++ app/src/main/java/com/neburizer/smartcaller/UpdateActivity.java
@@ -1,14 +1,20 @@
 package com.neburizer.smartcaller;
 
+import android.database.Cursor;
+import android.provider.CallLog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -17,10 +23,16 @@ import java.util.Map;
 
 public class UpdateActivity extends ActionBarActivity {
 
+    NumberPicker np;
+    String msg = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+        np = (NumberPicker) findViewById(R.id.np1);
+        np.setMaxValue(3);
+        np.setMinValue(1);
     }
 
 
@@ -46,52 +58,53 @@ public class UpdateActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void functionUpdateSettings(View v)
-    {
-        NumberPicker np = (NumberPicker)v.findViewById(R.id.numberPicker);
-        int npVals=np.getValue();
-        HashMap<String,String>[] days = new HashMap[np.getValue()];
-        days[0].put("9898989898","13");
-        days[0].put("8787878787","15");
-        days[0].put("7676767676", "18");
+    public void functionUpdateSettings(View v) {
 
-        days[1].put("9898989898", "13");
-        days[1].put("7676767676", "18");
-
-        days[2].put("9898989898", "13");
-        days[2].put("8787878787", "15");
-        days[2].put("7676767676", "21");
-        Iterator it = days[npVals-1].entrySet().iterator();
+        int npVals = np.getValue();
+        HashMap<String, Long>[] days = new HashMap[npVals + 1];
+        for (int j = 0; j <= npVals; j++)
+            days[j] = new HashMap<String, Long>();
+        Cursor logPointer = getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null, null, null);
+        while (logPointer.moveToNext()) {
+            String number = logPointer.getString(logPointer.getColumnIndex(CallLog.Calls.NUMBER));
+            long timeMils = Long.parseLong(logPointer.getString(logPointer.getColumnIndex(CallLog.Calls.DATE)));
+            Calendar c1 = Calendar.getInstance();
+            c1.setTimeInMillis(timeMils);
+            Log.i("Cols:-----------", "Number: " + number + " Time: " + c1.getTime());
+        }
+        Iterator it = days[npVals - 1].entrySet().iterator();
         ArrayList<String> passed = new ArrayList<String>();
-        while(it.hasNext())
-        {
-            Map.Entry E = (Map.Entry)it.next();
+        while (it.hasNext()) {
+            Map.Entry E = (Map.Entry) it.next();
             String key = (String) E.getKey();
             String val = (String) E.getValue();
             boolean flagVal = false;
-            for(int d=npVals-2;d>-1;d++)
-            {
-                HashMap<String,String> tempDay = days[d];
-                if(tempDay.containsKey(key))
-                {
-                    String tempVal = tempDay.get(key);
-                    if(tempVal.equals(val)){
-                       flagVal = true;
-                       continue;
+            for (int d = npVals; d > -1; d--) {
+                HashMap<String, Long> tempDay = days[d];
+                if (tempDay.containsKey(key)) {
+                    Long tempVal = tempDay.get(key);
+                    if (tempVal.equals(val)) {
+                        flagVal = true;
+                        continue;
+                    } else {
+                        flagVal = false;
+                        break;
                     }
-                }
-                else{
+                } else {
                     flagVal = false;
                     break;
                 }
             }
-            if (flagVal)
-            {
+            if (flagVal) {
                 passed.add(key);
             }
         }
+        System.out.println(" passed = " + passed.toString());
+
     }
-}/*
+}
+ /*
+    // Toast.makeText(getApplicationContext(), "input = " + npVals + " previous days", Toast.LENGTH_SHORT).show();/*
 import java.util.ArrayList;
         import java.util.HashMap;
         import java.util.Iterator;
